5f3046e71a3929fe2c4079c1ffa8e164fee692d6
Petar Lukovac <petar.lukovac@fon.bg.ac.rs>
2025-06-11 10:32:48 +0200

    Implementirano cuvanje korisnika u sesiji

--- index.php
+++ index.php
@@ -3,12 +3,17 @@
 require "dbBroker.php";
 require "model/user.php";
 
+session_start();
+
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $u = $_POST['username'];
     $p = $_POST['password'];
     $user = new User(null, $u, $p);
     $result = $user->loginUser($u, $p, $conn);
     if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $_SESSION['user_id'] = $row['id'];
+        //echo $_SESSION['user_id'];
         header('Location: home.php');
         exit();
     }
